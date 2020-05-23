3d9b4bc70923ce32136f2312472ecd67c5b6a655
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-05-23 22:20:16 +0530

    add an alert for deletion

--- app/src/main/java/com/example/to_doapp/adapter/TaskAdapter.java
+++ app/src/main/java/com/example/to_doapp/adapter/TaskAdapter.java
@@ -47,27 +47,38 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskViewHolder> {
             binding.doneStatus.setVisibility(View.GONE);
         }
         holder.itemView.findViewById(R.id.delete).setOnClickListener(v ->
-                EventBus.getDefault().post(new DeleteTaskEvent(task)));
+                openDeletionDialog(task));
 
         //If task is pending then add listener for updating the status to completed
         if ("pending".equals(taskList.get(position).getStatus())) {
-            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    openDialog(task, position);
-                    return true;
-                }
+            holder.itemView.setOnLongClickListener(v -> {
+                openUpdationDialog(task, position);
+                return true;
             });
         }
     }
 
-    private void openDialog(TaskModel task, int position) {
+    private void openUpdationDialog(TaskModel task, int position) {
         AlertDialog alertDialog = new AlertDialog.Builder(context).create();
         alertDialog.setMessage("Do you want to update this task as completed?");
         alertDialog.setCancelable(false);
         alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes",
                 (dialog, which) -> {
                     EventBus.getDefault().post(new UpdateStatusEvent(task, position));
+                    alertDialog.dismiss();
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No",
+                (dialog, which) -> alertDialog.dismiss());
+        alertDialog.show();
+    }
+
+    private void openDeletionDialog(TaskModel task) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setMessage("Are you sure you want to delete this task?");
+        alertDialog.setCancelable(false);
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes",
+                (dialog, which) -> {
+                    EventBus.getDefault().post(new DeleteTaskEvent(task));
                     alertDialog.dismiss();
                 });
         alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No",
